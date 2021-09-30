90981c46afa6d50e5973624391f6190e3c7ee200
Opanpan <fanalriansyah@gmail.com>
2021-10-01 01:01:12 +0700

    add cors package

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
