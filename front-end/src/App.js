4a78ab5a5238155292ac84a157ecc6fa1d3e6a4e
Opanpan <fanalriansyah@gmail.com>
2021-09-30 21:22:22 +0700

    init new frontend

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import "bootstrap/scss/bootstrap.scss";
+import Router from "./Router";
+
+function App() {
+  return (
+    <div className="bg">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
