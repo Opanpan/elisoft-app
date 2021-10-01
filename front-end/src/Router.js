37fd0c85d2e8eeaa8682661231cca41004ee61fd
Opanpan <fanalriansyah@gmail.com>
2021-10-01 11:23:52 +0700

    add path for /dashboard

--- front-end/src/Router.js
+++ front-end/src/Router.js
@@ -1,10 +1,15 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+import { isLogin } from "./utils";
 
 export default function MainRouter() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      {/* <ProtectedRoute path="/dashboard" component={Dashboard} exact /> */}
     </Switch>
   );
 }
